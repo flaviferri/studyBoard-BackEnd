ab8540f0cb99e6bf471bf829003c59257041645e
“Flavia <flaviferri90@gmail.com>
2024-09-20 09:25:54 +0200

    first commit

---
+++ src/main/java/com/sb/studyBoard_Backend/StudyBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sb.studyBoard_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBoardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/studyBoard_Backend/StudyBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.studyBoard_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
