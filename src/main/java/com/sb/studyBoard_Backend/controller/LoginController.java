8d0aa91f9560ce38eb85866c31161d42d9e5b074
“Flavia <flaviferri90@gmail.com>
2024-09-28 16:12:37 +0200

    github json given

--- src/main/java/com/sb/studyBoard_Backend/controller/LoginController.java
+++ src/main/java/com/sb/studyBoard_Backend/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
@@ -18,20 +17,15 @@ public class LoginController {
     private UserService userService;
 
     @GetMapping("/login/success")
-    public RedirectView loginSuccess(
-            @AuthenticationPrincipal OAuth2User user) {
-
+    public Map<String, Object> loginSuccess(@AuthenticationPrincipal OAuth2User user) {
         if (user == null) {
-            // Si el usuario no está autenticado, puedes redirigirlo a una página de error
-            return new RedirectView("/error");
+            throw new RuntimeException("User not authenticated");
         }
 
-        // Guardar los datos del usuario en la base de datos
         Map<String, Object> userAttributes = user.getAttributes();
         userService.saveOrUpdateUser(userAttributes);
 
-        // Redirigir a la página de inicio del frontend después de guardar al usuario
-        return new RedirectView("http://localhost:5173/home"); // Cambia la URL a la página de tu frontend
+        return userAttributes;
     }
 }
 
