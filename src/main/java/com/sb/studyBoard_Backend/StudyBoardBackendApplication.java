27d98f433eac11dc4d0002e7cfb7b5be04ace49b
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-30 09:30:22 +0200

    Adding .env file

--- src/main/java/com/sb/studyBoard_Backend/StudyBoardBackendApplication.java
+++ src/main/java/com/sb/studyBoard_Backend/StudyBoardBackendApplication.java
@@ -2,11 +2,18 @@ package com.sb.studyBoard_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class StudyBoardBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("GITHUB_CLIENTID", dotenv.get("GITHUB_CLIENTID"));
+		System.setProperty("GITHUB_CLIENT-SECRET", dotenv.get("GITHUB_CLIENT-SECRET"));
 		SpringApplication.run(StudyBoardBackendApplication.class, args);
 	}
 
