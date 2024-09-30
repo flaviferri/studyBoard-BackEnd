dfed9967aaa3884bda005ee3d13530505f6ae4ad
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-09-30 19:34:31 +0200

    fix conflicts

--- src/main/java/com/sb/studyBoard_Backend/config/SecurityConfig.java
+++ src/main/java/com/sb/studyBoard_Backend/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.Customizer;
 
 import org.springframework.security.web.SecurityFilterChain;
@@ -48,9 +46,9 @@ public class SecurityConfig {
         return source;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
 
 }
