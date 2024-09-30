f9f5b00dd08595bcd218b0c3405e52b249a790b0
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-30 22:34:51 +0200

    Configurations .env file

--- src/main/java/com/sb/studyBoard_Backend/StudyBoardBackendApplication.java
+++ src/main/java/com/sb/studyBoard_Backend/StudyBoardBackendApplication.java
@@ -6,16 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StudyBoardBackendApplication {
 
 	public static void main(String[] args) {
-	/*	Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.load();
 		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
 		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		System.setProperty("GITHUB_CLIENTID", dotenv.get("GITHUB_CLIENTID"));
-		System.setProperty("GITHUB_CLIENT-SECRET", dotenv.get("GITHUB_CLIENT-SECRET"));*/
+		System.setProperty("GITHUB_CLIENT-SECRET", dotenv.get("GITHUB_CLIENT-SECRET"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		SpringApplication.run(StudyBoardBackendApplication.class, args);
 	}
 
