3431ee113a2c29e01c059232cca10bc65a68c7f3
Betty Lopez <elisabetlopezpons@gmail.com>
2024-10-09 20:19:28 +0200

    BoardDTO

--- src/main/java/com/sb/studyBoard_Backend/dto/BoardDTO.java
+++ src/main/java/com/sb/studyBoard_Backend/dto/BoardDTO.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardDTO {
-    private Long id;
     private String title;
     private String color;
     private Long id;
